038627a74a10f330687066846a568489d8622284
刘冲 <liuchong@vmovier.com>
2018-08-19 18:10:16 +0800

    修改类文件名

--- NSTest1/NSTest/NSTest.h
+++ NSTest1/NSTest/NSTest.h
@@ -1,13 +1,15 @@
 //
-//  NSTest1.m
-//  NSTest1
+//  NSTest.h
+//  NSTest
 //
 //  Created by 刘冲 on 2018/8/18.
 //  Copyright © 2018年 lc. All rights reserved.
 //
 
-#import "NSTest1.h"
+#import <Foundation/Foundation.h>
 
-@implementation NSTest1
+@interface NSTest : NSObject
+
+- (void)test;
 
 @end
---
+++ NSTest1/NSTest/NSTest.m
@@ -0,0 +1,17 @@
+//
+//  NSTest.m
+//  NSTest
+//
+//  Created by 刘冲 on 2018/8/18.
+//  Copyright © 2018年 lc. All rights reserved.
+//
+
+#import "NSTest.h"
+
+@implementation NSTest
+
+- (void)test {
+    NSLog(@"test");
+}
+
+@end
